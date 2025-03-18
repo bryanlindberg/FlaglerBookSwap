39f7916d558c0d574b56bf73d200791f8a058649
Reuel Belizaire <Belizaire347@flagler.edu>
2025-03-18 16:15:01 -0400

    3/18

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FlaglerBookSwap.Views.Account
+namespace FlaglerBookSwap.Pages
 {
     public class LoginModel : PageModel
     {
