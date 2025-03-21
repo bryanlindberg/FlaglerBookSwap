ddb1fff646667e0884ee552b61f14f0c810eeacd
Reuel Belizaire <Belizaire347@flagler.edu>
2025-03-20 23:28:43 -0400

    Added validations for Login page but can't get login information to work 3/20/25

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,12 +1,60 @@
+using FlaglerBookSwap.Models;
+using FlaglerBookSwap.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace FlaglerBookSwap.Views.Account
+
+namespace FlaglerBookSwap.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+       
+        private readonly SignInManager<Users> signInManager;
+        private readonly UserManager<Users> userManager;
+        private readonly ILogger<LoginModel> logger; //help me troubleshoot how 
+
+        public LoginModel(UserManager<Users> userManager, SignInManager<Users> signInManager, ILogger<LoginModel> logger)
         {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.logger = logger;
+        }
+
+        [BindProperty]
+        public LoginViewModel LoginViewModel { get; set; }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.FindByEmailAsync(LoginViewModel.Email);
+                if (user == null)
+                {
+                    logger.LogWarning("User not found."); //confirms if email is in database
+                    ModelState.AddModelError(string.Empty, "User not found.");
+                    return Page();
+                }
+                var result = await signInManager.PasswordSignInAsync(LoginViewModel.Email, LoginViewModel.Password, LoginViewModel.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    logger.LogInformation("User logged in.");
+                    return RedirectToPage("/Index");
+                }
+                else
+                {
+                    logger.LogWarning("Password Is incorrect.");
+                    ModelState.AddModelError(string.Empty, "Password Is incorrect");
+                }
+            }
+            return Page();
         }
     }
 }
