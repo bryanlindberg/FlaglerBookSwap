0ab42182afefe719be31e6aa87fc711c0ac49131
Reuel Belizaire <Belizaire347@flagler.edu>
2025-03-23 22:43:38 -0400

    All pages are created 3/23/25

---
+++ Pages/Search & List/ManualList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlaglerBookSwap.Pages.Search___List
+{
+    public class ManualListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Search & List/ManualSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FlaglerBookSwap.Pages.Search___List
+{
+    public class ManualSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
