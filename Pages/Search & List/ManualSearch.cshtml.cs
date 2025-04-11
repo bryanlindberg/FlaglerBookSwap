32f79ec0afbb41bf88ae620f4f8d7c726339cedd
Bryan Lindberg <Lindberg266@flagler.edu>
2025-04-11 19:38:04 -0400

    4/11/2025, implemented manual search

--- Pages/Search & List/ManualSearch.cshtml.cs
+++ Pages/Search & List/ManualSearch.cshtml.cs
@@ -1,3 +1,5 @@
+using FlaglerBookSwap.Data;
+using FlaglerBookSwap.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +7,41 @@ namespace FlaglerBookSwap.Pages.Search___List
 {
     public class ManualSearchModel : PageModel
     {
+        [BindProperty]
+        public string? SearchOption { get; set; }
+        [BindProperty]
+        public string? SearchObject { get; set; }
+        public List<Textbooks> TextbookList { get; set; } = new List<Textbooks>();
+
+        private readonly AppDbContext _context;
+
+        public ManualSearchModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void OnPost()
+        {
+            if (SearchOption == "ISBN")
+            {
+                double isbnID = double.Parse(SearchObject);
+                TextbookList = _context.Textbooks
+                    .Where(t => t.ISBN == isbnID)
+                    .ToList();
+            }
+            if (SearchOption == "Textbook Title")
+            {
+                TextbookList = _context.Textbooks
+                    .Where(t => t.Book_Title.Contains(SearchObject))
+                    .ToList();
+            }
+            if (SearchOption == "Author")
+            {
+                TextbookList = _context.Textbooks
+                    .Where(t => t.Authors.Contains(SearchObject))
+                    .ToList();
+            }
+        }
         public void OnGet()
         {
         }
