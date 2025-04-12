633ed4972eca516a6538a3b6fa2f5631c6e6d268
Reuel Belizaire <Belizaire347@flagler.edu>
2025-04-11 22:44:41 -0400

    profile and display listing presentation looks good

--- Pages/Search & List/ListingDisplay.cshtml.cs
+++ Pages/Search & List/ListingDisplay.cshtml.cs
@@ -8,6 +8,12 @@ namespace FlaglerBookSwap.Pages.Search___List
 {
     public class ListingDisplayModel : PageModel
     {
+
+        public string? Book_Title { get; set; }
+        public double ISBN { get; set; }
+        public string? Authors { get; set; }
+
+
         [BindProperty(SupportsGet = true)]
         public short TextbookId { get; set; }
         public List<Listings> ListingDisplay { get; set; } = new List<Listings>();
@@ -23,12 +29,23 @@ namespace FlaglerBookSwap.Pages.Search___List
         public void OnGet(short textbookId)
         {
             TextbookId = textbookId;
+            //var textbookId = await _context
 
             ListingDisplay = _context.Listings
                 .Where(l => l.textbook_id == TextbookId)
                 .Include(l => l.Textbooks)
                 .Include(l => l.Users)
                 .ToList();
+
+            var textbook = _context.Textbooks.FirstOrDefault(t => t.textbook_id == TextbookId);
+            if (textbook != null)
+            {
+                Book_Title = textbook.Book_Title;
+                ISBN = textbook.ISBN;
+                Authors = textbook.Authors;
+            }
+
+
         }
     }
 }
